8d9b60ec0b3672a4c5ffdd4fee25c24be94cd005
Vaibhav <52103891+Martian-dev@users.noreply.github.com>
2025-04-30 14:41:09 +0530

    some changes idek

---
+++ passwordGenerator.py
@@ -0,0 +1,13 @@
+import random
+
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+symbols = "!@#$%^&*()_+-={}|[]\:\";'<>?,./"
+numbers = "0123456789"
+
+alphabets = alphabets.split()
+symbols = symbols.split()
+numbers = numbers.split()
+
+
+def randomizer(n):
+    pass
